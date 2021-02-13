963f6d3ce8a58b5fd4ca281e05bfad07451efe36
decorator-factory <42166884+decorator-factory@users.noreply.github.com>
2021-02-13 20:50:23 +0300

    Add more functions, including IO

--- src/lang/interpreter.ts
+++ src/lang/interpreter.ts
@@ -21,13 +21,14 @@ export type Env = {
 
 export type NativeFn = (v: Value, e: Env) => Value;
 
-
+type FunT = {fun: LamT, closure: Env};
+type LazyName = string | (() => string);
 export type Value =
   | {str: string}
   | {num: number}
   | {symbol: string}
-  | {fun: LamT, closure: Env}
-  | {native: NativeFn, name: string}
+  | FunT
+  | {native: NativeFn, name: LazyName}
   | {table: Map<string, Value>}
   | {bool: boolean}
 
@@ -38,7 +39,7 @@ const Num = (num: number): Value => ({num});
 const Bool = (bool: boolean): Value => ({bool});
 const Table = (table: Map<string, Value>): Value => ({table});
 const Fun = (fun: LamT, closure: Env): Value => ({fun, closure});
-const Native = (name: string, native: (v: Value, e: Env) => Value): Value => ({name, native});
+const Native = (name: LazyName, native: (v: Value, e: Env) => Value): Value => ({name, native});
 
 
 const asNum = (v: Value): number => {
@@ -59,6 +60,26 @@ const asSymb = (v: Value): string => {
   return v.symbol;
 };
 
+const asStrOrSymb = (v: Value): string => {
+  if ('str' in v)
+    return v.str;
+  if ('symbol' in v)
+    return v.symbol;
+  throw new Error(`Expected string or symbol, got ${prettyPrint(v)}`);
+};
+
+const asTable = (v: Value): Map<string, Value> => {
+  if (!('table' in v))
+    throw new Error(`Expected table, got ${prettyPrint(v)}`);
+  return v.table;
+};
+
+const asFun = (v: Value): FunT => {
+  if (!('fun' in v))
+    throw new Error(`Expected function, got ${prettyPrint(v)}`);
+  return v;
+};
+
 
 const envRepr = (env: Env, keys: string[]): string => {
   const lines: string[] = [];
@@ -76,7 +97,7 @@ export const prettyPrint = (v: Value): string => {
   else if ('fun' in v)
     return `${unparse(v.fun)} where ${envRepr(v.closure, v.fun.capturedNames)}`;
   else if ('native' in v)
-    return v.name;
+    return typeof v.name === 'string' ? v.name : v.name();
   else if ('symbol' in v)
     return '.' + v.symbol;
   else if ('bool' in v)
@@ -118,27 +139,144 @@ export const interpret = (expr: Expr, env: Env): Value => {
 
 const compose = (f1: Value, f2: Value): Value =>
   Native(
-    `(${prettyPrint(f1)} . ${prettyPrint(f2)})`,
+    `(${prettyPrint(f2)} >> ${prettyPrint(f1)})`,
     (arg: Value, env: Env) => applyFunction(f1, applyFunction(f2, arg, env), env)
   );
 
 
-const GLOBAL_ENV: Env = {
-  parent: null,
-  names: Map({
-    '+': Native('(+)', a => Native(`(${prettyPrint(a)} +)`, b => Num(asNum(a) + asNum(b)))),
-    '-': Native('(-)', a => Native(`(${prettyPrint(a)} -)`, b => Num(asNum(a) - asNum(b)))),
-    '*': Native('(*)', a => Native(`(${prettyPrint(a)} *)`, b => Num(asNum(a) * asNum(b)))),
-    '^': Native('(^)', a => Native(`(${prettyPrint(a)} ^)`, b => Num(Math.pow(asNum(a), asNum(b))))),
-    '++': Native('(++)', a => Native(`(${prettyPrint(a)} ++)`, b => Str(asStr(a) + asStr(b)))),
-    '<<': Native('(<<)', a => Native(`(${prettyPrint(a)} <<)`, b => compose(a, b))),
-    '>>': Native('(>>)', a => Native(`(${prettyPrint(a)} >>)`, b => compose(b, a))),
-    '|>': Native('(|>)', a => Native(`(${prettyPrint(a)} |>)`, (f, env) => applyFunction(f, a, env))),
-    'true': Bool(true),
-    'false': Bool(false),
-  })
-};
+const GLOBAL_ENV: Env = (() => {
+  const _dumpEnv = (env: Env, depth: number = 0) => {
+    [...env.names.entries()].sort().forEach(
+      ([k, v]) => console.log('  '.repeat(depth) + `${k} : ${prettyPrint(v)}`)
+    );
+    if (env.parent !== null)
+      _dumpEnv(env.parent, depth + 1);
+  };
 
+  const unit: Value = Table(Map({}));
+
+  const _fallback = Native(
+    '(|?)',
+    tableV => Native(
+      () => `(${prettyPrint(tableV)} |?)`,
+      (fallbackV) => Native(
+        () => `(${prettyPrint(tableV)} |? ${prettyPrint(fallbackV)})`,
+        (key, env) => {
+          if (!('table' in tableV))
+            return applyFunction(tableV, key, env);
+          const table = asTable(tableV);
+          if (!('symbol' in key))
+            return applyFunction(fallbackV, key, env);
+          const rv = table.get(key.symbol);
+          if (rv === undefined)
+            return applyFunction(fallbackV, key, env);
+          return rv;
+        }
+    )
+  ));
+
+  const ioFunctions = {
+    'log': Native(
+      'IO.log',
+      s => {
+        console.log(asStr(s));
+        return unit;
+      }
+    ),
+
+    'debug': Native(
+      'IO.debug',
+      a => {
+        console.log(prettyPrint(a));
+        return a;
+    }),
+
+    'define': Native(
+      'IO.define',
+      s => Native(
+        `(IO.define ${prettyPrint(s)})`,
+        v => {
+          GLOBAL_ENV.names = GLOBAL_ENV.names.set(asStrOrSymb(s), v);
+          return v;
+        }
+      )
+    ),
+
+    'forget': Native(
+      'IO.forget',
+      s => {
+        GLOBAL_ENV.names = GLOBAL_ENV.names.delete(asStrOrSymb(s));
+        return unit;
+      }
+    ),
+
+    'try': Native(
+      'IO.try',
+      (fn, env) => {
+        try {
+          return Table(Map({
+            ok: applyFunction(fn, unit, env)
+          }));
+        } catch (e) {
+          return Table(Map({
+            error: Str(`${e}`)
+          }));
+        }
+      }
+    ),
+
+    'locals': Native(
+      'IO.locals',
+      (_, env) => {
+        _dumpEnv(env);
+        return unit;
+      }
+    )
+  };
+
+  return {
+    parent: null,
+    names: Map({
+      '+': Native('(+)', a => Native(`(${prettyPrint(a)} +)`, b => Num(asNum(a) + asNum(b)))),
+      '-': Native('(-)', a => Native(`(${prettyPrint(a)} -)`, b => Num(asNum(a) - asNum(b)))),
+      '*': Native('(*)', a => Native(`(${prettyPrint(a)} *)`, b => Num(asNum(a) * asNum(b)))),
+      '^': Native('(^)', a => Native(`(${prettyPrint(a)} ^)`, b => Num(Math.pow(asNum(a), asNum(b))))),
+      '++': Native('(++)', a => Native(() => `(${prettyPrint(a)} ++)`, b => Str(asStr(a) + asStr(b)))),
+      '<<': Native('(<<)', a => Native(() => `(${prettyPrint(a)} <<)`, b => compose(a, b))),
+      '>>': Native('(>>)', a => Native(() => `(${prettyPrint(a)} >>)`, b => compose(b, a))),
+      '|>': Native('(|>)', a => Native(() => `(${prettyPrint(a)} |>)`, (f, env) => applyFunction(f, a, env))),
+      'true': Bool(true),
+      'false': Bool(false),
+
+      'fallback': _fallback,
+      '|?': _fallback,
+
+      'IO': Table(Map(ioFunctions)),
+
+      'given': Native(
+        'given',
+        namesV => Native(
+          () => `(given ${prettyPrint(namesV)})`,
+          (funV, env) => {
+            const table = asTable(namesV);
+            const fun = asFun(funV);
+
+            const newClosure: Env = {
+              parent: fun.closure,
+              names: table
+            };
+
+            const newFun: FunT = {
+              fun: fun.fun,
+              closure: newClosure
+            };
+            return applyFunction(newFun, unit, env);
+          }
+        )
+      )
+    })
+  };
+})();
 
 export const PARSER = makeParser({
   priorities: {
@@ -149,9 +287,10 @@ export const PARSER = makeParser({
     '++': Prio(10, 'left'),
     '<<': Prio(3, 'right'),
     '>>': Prio(3, 'left'),
-    '|>': Prio(2, 'left')
+    '|>': Prio(2, 'left'),
+    '|?': Prio(3, 'right'),
   },
-  namePriority: Prio(20, 'left'),
+  namePriority: Prio(20, 'right'),
   defaultPriority: Prio(5, 'left'),
 });
 
